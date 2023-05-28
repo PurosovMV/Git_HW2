9546f6baa1683ea0bbf684ada08b03230e394774
PurosovMV <mihail.purosov@gmail.com>
2023-05-28 11:45:50 +0300

    Revert "add method average()"
    This reverts commit a7d979a96d179111a44496d2050e0c2f5f4cfb49.

--- task001.java
+++ task001.java
@@ -10,7 +10,6 @@ public class task001 {
         System.out.println(arr);
         System.out.println(max(array()));
         System.out.println(min(array()));
-        average(array());
 
     }
 
@@ -44,15 +43,6 @@ public class task001 {
 
         }
         return min;
-    }
-
-    static void average(double[] array) {
-        double avg = 0;
-        for (int i = 0; i < array.length; i++) {
-
-            avg += array[i] / array.length;
-        }
-        System.out.println(avg);
     }
 
     static int randomNumbers() {
